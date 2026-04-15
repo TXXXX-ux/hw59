0db6dc08543a8539c1334a80ea801ee139eb4362
Канат <kanat016@kstu.kg>
2026-04-15 18:05:58 +0600

    default

---
+++ src/main/java/kg/attractor/lesson55lab/Lesson55labApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.lesson55lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson55labApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson55labApplication.class, args);
+	}
+
+}
