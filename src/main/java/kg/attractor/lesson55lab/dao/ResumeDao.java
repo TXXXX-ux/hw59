4121730e52d18603ae5f328964cbd4d564833165
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-16 11:55:22 +0600

    Update ResumeDao.java

--- src/main/java/kg/attractor/lesson55lab/dao/ResumeDao.java
+++ src/main/java/kg/attractor/lesson55lab/dao/ResumeDao.java
@@ -1,27 +1,7 @@
 package kg.attractor.lesson55lab.dao;
 
 import kg.attractor.lesson55lab.model.Resume;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-@Repository
-public class ResumeDao {
-    private final JdbcTemplate jdbcTemplate;
-
-    public ResumeDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Resume> findAll() {
-        String sql = "SELECT * FROM resumes";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class));
-    }
-
-    public void save(Resume resume) {
-        String sql = "INSERT INTO resumes (title, description) VALUES (?, ?)";
-        jdbcTemplate.update(sql, resume.getTitle(), resume.getDescription());
-    }
+public interface ResumeDao extends JpaRepository<Resume, Long> {
 }
