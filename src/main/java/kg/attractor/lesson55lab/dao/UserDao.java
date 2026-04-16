b73bb2215910fe5313399d9d742f9881f47476fb
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-16 11:55:37 +0600

    Update UserDao.java

--- src/main/java/kg/attractor/lesson55lab/dao/UserDao.java
+++ src/main/java/kg/attractor/lesson55lab/dao/UserDao.java
@@ -1,37 +1,9 @@
 package kg.attractor.lesson55lab.dao;
 
 import kg.attractor.lesson55lab.model.User;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-import java.util.List;
-
-@Repository
-public class UserDao {
-    private final JdbcTemplate jdbcTemplate;
-
-    public UserDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<User> findAll() {
-        String sql = "SELECT * FROM users";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
-    }
-
-    public User findByEmail(String email) {
-        String sql = "SELECT * FROM users WHERE email = ?";
-        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class), email);
-        if (users.isEmpty()) {
-            return null;
-        }
-        return users.get(0);
-    }
-
-    public void save(User user) {
-        String sql = "INSERT INTO users (email, password, name, account_type, about, avatar) VALUES (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, user.getEmail(), user.getPassword(), user.getName(),
-                user.getAccountType().name(), user.getAbout(), user.getAvatar());
-    }
+public interface UserDao extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
