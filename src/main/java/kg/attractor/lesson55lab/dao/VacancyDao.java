a495ad91427d3eeddab5ee369a1e4fe515cace17
TXXXX-ux <mambetovkanat7@gmail.com>
2026-04-16 21:24:45 +0600

    Update VacancyDao.java

--- src/main/java/kg/attractor/lesson55lab/dao/VacancyDao.java
+++ src/main/java/kg/attractor/lesson55lab/dao/VacancyDao.java
@@ -1,27 +1,7 @@
 package kg.attractor.lesson55lab.dao;
 
 import kg.attractor.lesson55lab.model.Vacancy;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-@Repository
-public class VacancyDao {
-    private final JdbcTemplate jdbcTemplate;
-
-    public VacancyDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Vacancy> findAll() {
-        String sql = "SELECT * FROM vacancies";
-        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Vacancy.class));
-    }
-
-    public void save(Vacancy vacancy) {
-        String sql = "INSERT INTO vacancies (title, description, salary) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, vacancy.getTitle(), vacancy.getDescription(), vacancy.getSalary());
-    }
+public interface VacancyDao extends JpaRepository<Vacancy, Long> {
 }
